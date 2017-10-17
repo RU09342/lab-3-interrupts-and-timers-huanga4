ac91232f878f537d2d0a852262527fff4786087a
huanga4 <32047022+huanga4@users.noreply.github.com>
2017-10-16 20:16:20 -0400

    Update main.c

--- Button Interrupt/Button Interrupt(2553)/main.c
+++ Button Interrupt/Button Interrupt(2553)/main.c
@@ -5,30 +5,30 @@
  * main.c
  */
 int main(void) {
-    WDTCTL = WDTPW | WDTHOLD;// Stop watchdog timer
+    WDTCTL = WDTPW | WDTHOLD;// Stop watchdog 
 
-    P1SEL =0; //Select GPIO option
+    P1SEL =0; //select GPIO 
     P1DIR |=BIT0; //set Port 1.0 output
 
     P1DIR &=~(BIT3); //set Port 1.3 input
     P1REN|=BIT3;
     P1OUT|=BIT3;
 
-    P1IE |=BIT3;//enable the interrupt on Port 1.1
-    P1IES |=BIT3;//set as falling edge
-    P1IFG &=~(BIT3);//clear interrupt flag
+    P1IE |=BIT3;//enable interrupt
+    P1IES |=BIT3;//set falling edge
+    P1IFG &=~(BIT3);//clear flag
 
     //lpm
     _BIS_SR(LPM4_bits + GIE);
 }
-//Port 1 ISR
+
 #pragma vector=PORT1_VECTOR
 __interrupt void PORT_1(void)
 {
-    P1OUT ^=0x01; // Toggle P1.1
-    //Debouncing
+    P1OUT ^=0x01; // Toggle
+    //Debounce
     volatile unsigned int duration = 500;
     do (duration--);
     while (duration != 0);
-    P1IFG &=~(BIT3); // Clear flag
+    P1IFG &=~(BIT3); // clear flag
 }
