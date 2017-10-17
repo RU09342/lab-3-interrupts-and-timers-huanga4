5b890befb80526461da457e5008171a3f523ea35
huanga4 <32047022+huanga4@users.noreply.github.com>
2017-10-16 20:16:18 -0400

    Update main.c

--- Button Interrupt/Button Interrupt(5529)/main.c
+++ Button Interrupt/Button Interrupt(5529)/main.c
@@ -7,30 +7,30 @@
  * Button Interrupt 5529
  */
 void main(void) {
-    WDTCTL = WDTPW | WDTHOLD;// Stop watchdog timer
+    WDTCTL = WDTPW | WDTHOLD;// Stop watchdog
 
     P1SEL =0;
     P1DIR |=BIT0;
 
     P1DIR &=~(BIT1);
-    P1REN|=BIT1;//enable pull-up/pull-down resistor on
-    P1OUT|=BIT1; //choose the pull-up resistor
+    P1REN|=BIT1;//enable pull-up/pull-down resistor
+    P1OUT|=BIT1; //choose pull-up 
 
-    P1IE |=BIT1;//enable the interrupt on Port 1.1
-    P1IES |=BIT1;//set as falling edge
-    P1IFG &=~(BIT1);//clear interrupt flag
+    P1IE |=BIT1;//enable interrupt
+    P1IES |=BIT1;//set falling edge
+    P1IFG &=~(BIT1);//clear flag
 
-    //enter low power mode
-    _BIS_SR(LPM4_bits + GIE);
+    
+    _BIS_SR(LPM4_bits + GIE); //low power mode
 }
-//Port 1 ISR
+
 #pragma vector=PORT1_VECTOR
 __interrupt void PORT_1(void)
 {
-    P1OUT ^=0x01; // Toggle P1.1
-    //Debouncing
+    P1OUT ^=0x01; // Toggle 
+    //Debounce
     volatile unsigned int duration = 500;
     do (duration--);
     while (duration != 0);
-    P1IFG &=~(BIT1); // Clear flag
+    P1IFG &=~(BIT1); // clear flag
 }
