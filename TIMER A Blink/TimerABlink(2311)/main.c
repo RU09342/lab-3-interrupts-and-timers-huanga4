abe53d133aabb93a7b63ef513673b38d8251e805
huanga4 <32047022+huanga4@users.noreply.github.com>
2017-10-16 20:31:07 -0400

    Update main.c

--- TIMER A Blink/TimerABlink(2311)/main.c
+++ TIMER A Blink/TimerABlink(2311)/main.c
@@ -10,34 +10,31 @@ int main(void) {
     WDTCTL = WDTPW | WDTHOLD;// Stop watchdog timer
 
     PM5CTL0 &= ~LOCKLPM5;                   // Disable the GPIO power-on default high-impedance mode
-                                            // to activate previously configured port settings
-
+                                            
     P1OUT &= ~(BIT0); // Clear P1.0
-    P1DIR |=BIT0; //set Port 1.0 output ---LED
+    P1DIR |=BIT0; //set Port 1.0 LED
 
     P2OUT &= ~(BIT0); //Clear P2.0
-    P2DIR |=BIT0; //set Port 2.0 output ---LED
+    P2DIR |=BIT0; //set Port 2.0 LED
 
-
-    TB0CTL= TBSSEL_1 + MC_1; //Set up Timer B0 in Up mode and use ACLK
-    TB0CCTL0 = 0x10; //Set up Timer B0 in compare mode
-    TB0CCR0 = 12000; //set up TB0CCR0, and have Timer B0 (TB0R) to count to its contents.
-                    // 32kHz/12000 is the frequency at which the LED will blink
-    TB0CCTL1 = 0x10;    // Set up Timer B 1 Compare mode
-    TB0CCR1 = 3000;     // Set up value for CCR1 to 3000
+    TB0CTL= TBSSEL_1 + MC_1; //Set up Timer B0, Up mode, ACLK
+    TB0CCTL0 = 0x10; //set up compare mode
+    TB0CCR0 = 12000; //set up TB0CCR0; LED frequency = 32kHz/12000 
+    TB0CCTL1 = 0x10;    // Set up compare mode
+    TB0CCR1 = 3000;     // CCR1 assigned value
     //enter LPM4 mode and enable global interrupt
     _BIS_SR(LPM4_bits + GIE);
 
 }
-//Port 1 ISR
+
 #pragma vector=TIMER0_B0_VECTOR
 #pragma vector = TIMER0_B1_VECTOR
 __interrupt void Timer_B(void)
 {
-    P1OUT ^= 0x01; //Toggle the P1.0 LED
-    switch (TB0IV) //Choose the right Interrupt Vector for value of TB0CCR1
+    P1OUT ^= 0x01; //Toggle 
+    switch (TB0IV) //choose correct value for corresponding interrupt
     {
-    case TB0IV_TBCCR1: P2OUT ^= BIT0; //Toggle the P2.0 LED
+    case TB0IV_TBCCR1: P2OUT ^= BIT0; //Toggle
     break;
     }
 }
